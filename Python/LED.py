8b3b5038ddbce1340db02ad46f42b410cf5f5532
kherhozen <62188308+kherhozen@users.noreply.github.com>
2025-08-19 21:47:16 +0200

    Update LED.py to python3

--- Python/LED.py
+++ Python/LED.py
@@ -7,13 +7,12 @@ Example: Control the RGB LED onboard of Navio shield for Raspberry Pi
 Please note that this example uses Adafruit I2C and PCA9685 drivers.
 
 To run this example navigate to the directory containing it and run following commands:
-sudo python LED.py
+sudo python3 LED.py
 """
 
-#from Adafruit_PWM_Servo_Driver import PWM
+# from Adafruit_PWM_Servo_Driver import PWM
 from navio.adafruit_pwm_servo_driver import PWM
 import time
-
 import sys
 
 import navio.gpio
@@ -21,7 +20,7 @@ import navio.util
 
 navio.util.check_apm()
 
-#drive Output Enable in PCA low
+# drive Output Enable in PCA low
 pin = navio.gpio.Pin(27)
 pin.write(0)
 
@@ -30,51 +29,49 @@ pwm = PWM(0x40, debug=False)
 # Set frequency to 60 Hz
 pwm.setPWMFreq(60)
 
-step = 1 # light color changing step
+step = 1  # light color changing step
 
 # set initial color
 R = 0
 G = 0
 B = 4095
 pwm.setPWM(0, 0, B)
-print "LED is yellow"
+print("LED is yellow")
 time.sleep(1)
 
-while (True):
-    for R in xrange(0, 4095, step):
+while True:
+    for R in range(0, 4095, step):
         pwm.setPWM(2, 0, R)
-    print "LED is green"
+    print("LED is green")
     time.sleep(1)
 
-    for B in xrange(4095, 0, -step):
+    for B in range(4095, 0, -step):
         pwm.setPWM(0, 0, B)
-    print "LED is cyan"
-    time.sleep(1)
-    
-    for G in xrange(0, 4095, step):
-        pwm.setPWM(1, 0, G)
-    print "LED is blue"
+    print("LED is cyan")
     time.sleep(1)
 
-    for R in xrange(4095, 0, -step):
+    for G in range(0, 4095, step):
+        pwm.setPWM(1, 0, G)
+    print("LED is blue")
+    time.sleep(1)
+
+    for R in range(4095, 0, -step):
         pwm.setPWM(2, 0, R)
-    print "LED is magenta"
+    print("LED is magenta")
     time.sleep(1)
 
-    for B in xrange(0, 4095, step):
+    for B in range(0, 4095, step):
         pwm.setPWM(0, 0, B)
-    print "LED is red"
+    print("LED is red")
     time.sleep(1)
 
-    for G in xrange(4095, 0, -step):
+    for G in range(4095, 0, -step):
         pwm.setPWM(1, 0, G)
-    print "LED is yellow"
+    print("LED is yellow")
     time.sleep(1)
 
-  # Change speed of continuous servo on channel O
-  # pwm.setPWM(0, 0, servoMin)
-  # time.sleep(1)
-  # pwm.setPWM(0, 0, servoMax)
-  # time.sleep(1)
-
-
+    # Change speed of continuous servo on channel 0
+    # pwm.setPWM(0, 0, servoMin)
+    # time.sleep(1)
+    # pwm.setPWM(0, 0, servoMax)
+    # time.sleep(1)
